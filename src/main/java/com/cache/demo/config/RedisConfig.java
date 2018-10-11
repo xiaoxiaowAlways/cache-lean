89681689f54e2e2d9b597f874a867534f76a785d
Will Wang <wilwang3@wkmcn6358508.global.publicisgroupe.net>
2018-10-11 09:43:23 +0800

    auto

--- src/main/java/com/cache/demo/config/RedisConfig.java
+++ src/main/java/com/cache/demo/config/RedisConfig.java
@@ -1,19 +1,13 @@
 package com.cache.demo.config;
 
-import com.cache.demo.model.User;
-
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-
-import java.util.List;
 
 /**
  * @Auther: Will Wang 3
@@ -24,23 +18,7 @@ import java.util.List;
 public class RedisConfig {
 
   @Bean
-  public RedisTemplate<Object, User> userRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
-    RedisTemplate<Object, User> template = new RedisTemplate<>();
-    template.setConnectionFactory(redisConnectionFactory);
-    template.setDefaultSerializer(new Jackson2JsonRedisSerializer<>(User.class));
-    return template;
-  }
-
-  @Primary
-  @Bean
-  public RedisCacheManager userCacheManager(RedisTemplate<Object, User> userRedisTemplate) {
-    RedisCacheManager cacheManager = new RedisCacheManager(userRedisTemplate);
-    cacheManager.setUsePrefix(true);
-    return cacheManager;
-  }
-
-  @Bean
-  public RedisTemplate<Object, Object> userListRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+  public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
     RedisTemplate<Object, Object> template = new RedisTemplate<>();
     template.setConnectionFactory(redisConnectionFactory);
     Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
@@ -52,8 +30,8 @@ public class RedisConfig {
   }
 
   @Bean
-  public RedisCacheManager userListCacheManager(RedisTemplate<Object, Object> userListRedisTemplate) {
-    RedisCacheManager cacheManager = new RedisCacheManager(userListRedisTemplate);
+  public RedisCacheManager cacheManager(RedisTemplate<Object, Object> redisTemplate) {
+    RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
     cacheManager.setUsePrefix(true);
     return cacheManager;
   }
--- src/main/java/com/cache/demo/service/UserService.java
+++ src/main/java/com/cache/demo/service/UserService.java
@@ -2,11 +2,9 @@ package com.cache.demo.service;
 
 import com.cache.demo.model.User;
 
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,13 +19,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class UserService {
 
-  @Cacheable(cacheNames = "user", cacheManager = "userCacheManager")
+  @Cacheable(cacheNames = "user")
   public User getUserById(Integer userId) {
     log.info("getUserById 被调用。。。。");
     return new User(userId, "xiaoxiaow" + userId);
   }
 
-  @Cacheable(cacheNames = "allUser", cacheManager = "userListCacheManager")
+  @Cacheable(cacheNames = "allUser")
   public List<User> getAllUser(Integer type) {
     log.info("getAllUser 被调用。。。。");
     User user1 = new User(1, "xiaoxiaow1");
